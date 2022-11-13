19bcd5b79b7c2f141f785b34c976bf8470b0f8fe
tutncrusoe <huydinhanhle@gmail.com>
2022-11-13 23:57:21 +0700

    Class

---
+++ Class/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // create an object of the outer class Car
+        Car car1 = new Car("Mazda", "8WD");
+
+        // create an object of inner class using the outer class
+        Car.Engine engine = car1.new Engine();
+        engine.setEngine();
+        System.out.println("Engine Type for 8WD= " + engine.getEngineType());
+
+        Car car2 = new Car("Crysler", "4WD");
+        Car.Engine c2engine = car2.new Engine();
+        c2engine.setEngine();
+        System.out.println("Engine Type for 4WD = " + c2engine.getEngineType());
+
+    }
+}
