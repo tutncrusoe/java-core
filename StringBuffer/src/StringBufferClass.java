8b4866a56e6e336affe1ec1b6ec737216b5b8140
tutncrusoe <huydinhanhle@gmail.com>
2022-12-14 09:58:39 +0700

    StringBuffer

--- StringBuffer/src/StringBufferClass.java
+++ StringBuffer/src/StringBufferClass.java
@@ -1,3 +1,7 @@
+/**
+ * https://loda.me/articles/jav2-bien-phm-vi-kiu-d-liu-ton-t-trong-java
+ * https://loda.me/articles/vi-sao-nen-su-dung-stringbuffer
+ */
 public class StringBufferClass {
 
     public static void main(String[] args) {
@@ -43,4 +47,17 @@ public class StringBufferClass {
         // Thì nó chỉ cần gắn chuỗi bytes của "B" vào liền kề ngay sau "A" là xong.
         // (Vì nó có thể thay đổi, khác với String là immutable).
     }
+    /**
+     * Bản chất của biến (Nói thêm)
+     * Khi các bạn khai báo một biến int trong chương trình của mình và sử dụng lung tung khắp mọi nơi, thì bạn có biết cái biến int ý ở đâu lòi ra không :))
+     *
+     * Về bản chất, Biến sẽ là một vùng nhớ trong thiết bị vật lý mà dễ nhất là để trong ram. và khi bạn cho nó một giá trị, nó sẽ lưu trữ số đó vào ram, và cần thì lấy lên.
+     *
+     * Vậy để ram biết bạn muốn lưu cái gì thì bạn phải khai báo cho nó. Ví dụ bạn bảo tôi cần một số nguyên int. Thì máy tính hiểu là mình cần lưu trữ một số nguyên bình thường, không quá lớn, nó sẽ cho bạn 4 byte trong Ram thích lưu gì thì lưu. nhưng không được vượt quá 4 byte.
+     *
+     * 4 byte = 32 bit, bỏ đi 1 bit đầu tiên để đánh dấu là số âm hay dương, thì còn 31 bit => số lớn nhất mà biến int lưu trữ được là 2^31 - 1 = 2147483647
+     * Từ đây, bạn sẽ hiểu vì sao có số long, vì nhu cầu lưu số lớn hơn thì long được cấp tận 8 byte.
+     *
+     * Còn trường hợp đặc biệt như String thì tuỳ giá trị của nó có bao nhiêu ký tự, mà Ram sẽ cấp tương ứng bấy nhiêu byte
+     */
 }
